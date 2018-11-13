1fb02aee2949501bf90718fa743f3c9373b14aec
bsw <bswsbmail@gmail.com>
2018-11-13 23:50:20 +0800

    第一题

---
+++ 001.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in nums:
+            for j in range(nums.index(i)+1,len(nums)):
+                sums = i + nums[j]
+                if sums == target:
+                    return [nums.index(i),j]
