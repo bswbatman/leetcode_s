fa4c3386ac058f2e14c3418f98335207b52f3c8d
bsw <bswsbmail@gmail.com>
2018-11-17 00:14:59 +0800

    简单

--- 001.py
+++ 001.py
@@ -66,7 +66,7 @@ class Solution:
             a = False
         return a
 
-#--13
+#--13  #cant use
 class Solution:
     def romanToInt(self, s):
         """
@@ -87,10 +87,14 @@ class Solution:
         listt = list(s)
         lenn = len(listt)
         k = 0
+
+        j = 0
         for i in range(lenn):
             print('--------------------------------------')
             print('i1', i)
             i += k
+            if j - i == 1:
+                i = j + 1
             # print(listt)
 
             print('i2', i)
@@ -107,18 +111,23 @@ class Solution:
                 # print('i',i)
                 k += 1
                 if summ == 0:
-                    summm += 1
+                    summm += dii[listt[i]]
                     print('summm:', summm)
                     k = 0
                 print('sum1', summm)
                 print('k:', k)
                 print('i:', i)
+                j = i
             else:
                 k = 0
                 print('strart')
                 summm += dii[listt[i]]
+                # if dii[listt[i]] > dii[listt[i+1]]:
+                #     k = 1
                 print('make：', listt[i])
                 print('sum2', summm)
                 print('k:', k)
                 print('i:', i)
+                j = i
+
         return summm
